bad6adfa3d694e28e3830f28fdb4459434030e1e
kurtgalvin <45803439+kurtgalvin@users.noreply.github.com>
2019-09-30 18:55:29 -0700

    moving ball

--- ball_game/index.js
+++ ball_game/index.js
@@ -7,26 +7,68 @@ function onMouseMove(e, myGoal) {
   myGoal.y = e.clientY
 }
 
-function gameLoop(canvas, context, myCircle, myGoal) {
+function gameLoop(canvas, context, myCircle, myGoal, myClick) {
   clearCanvas(context, canvas);
-  console.log(myCircle.x, myGoal.x)
+  let moveMultiple = 3
+  if(myClick) {
+    moveMultiple = moveMultiple*5
+  }
+
+  let testX = myGoal.x - myCircle.x;
+  let testY = myGoal.y - myCircle.y;
+  let revX = false;
+  let revY = false;
+
+  if(testX < 0) {
+    testX = testX*-1;
+    revX = true;
+  }
+  if(testY < 0) {
+    testY = testY*-1;
+    revY = true;
+  }
+
+  if(revX && revY) {
+    myCircle.x = myCircle.x + -(testX/(testX+testY))*moveMultiple
+    myCircle.y = myCircle.y + -(testY/(testX+testY))*moveMultiple
+  } else if(revX) {
+    myCircle.x = myCircle.x + -(testX/(testX+testY))*moveMultiple
+    myCircle.y = myCircle.y + (testY/(testX+testY))*moveMultiple
+  } else if(revY) {
+    myCircle.x = myCircle.x + (testX/(testX+testY))*moveMultiple
+    myCircle.y = myCircle.y + -(testY/(testX+testY))*moveMultiple
+  } else {
+    myCircle.x = myCircle.x + (testX/(testX+testY))*moveMultiple
+    myCircle.y = myCircle.y + (testY/(testX+testY))*moveMultiple
+  }
+
   context.beginPath();
-  context.arc(myCircle.x, myCircle.y, 10, 0, 2*Math.PI, false);
-  context.fillStyle = 'green';
+  if(myClick){
+    context.arc(myCircle.x, myCircle.y, 12, 0, 2*Math.PI, false);
+    context.fillStyle = 'lightblue';
+  } else {
+    context.arc(myCircle.x, myCircle.y, 10, 0, 2*Math.PI, false);
+    context.fillStyle = 'green';
+  }
   context.fill();
 }
 
 (function main() {
   const canvas = document.getElementById("gameCanvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   const context = canvas.getContext('2d');
   let myCircle = {
-    x: 0,
-    y: 0
+    x: 50,
+    y: 50
   };
   let myGoal = {
     x: 0,
     y: 0
   };
-  canvas.onmousemove = (e) => onMouseMove(e, myGoal);
-  setInterval(() => gameLoop(canvas, context, myCircle, myGoal), 33); // ~ 30 frames per sec
+  onmousemove = (e) => onMouseMove(e, myGoal);
+  let myClick = false
+  canvas.onmousedown = () => {myClick = true}
+  canvas.onmouseup = () => {myClick = false}
+  setInterval(() => gameLoop(canvas, context, myCircle, myGoal, myClick), 33); // ~ 30 frames per sec
 })();
